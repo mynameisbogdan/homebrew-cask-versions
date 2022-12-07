fc754e4ced0189e959e2773cac8f45f313d590ff
Bogdan <mynameisbogdan@users.noreply.github.com>
2022-12-07 02:18:39 +0200

    add qbittorrent44

---
+++ Casks/qbittorrent44.rb
@@ -0,0 +1,24 @@
+cask "qbittorrent44" do
+  version "4.4.5"
+  sha256 "f61fa0140114d2d2ab96d3a39ce28b7f295b6c1ba7199aaf3f342881d3fdb903"
+
+  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg",
+      verified: "sourceforge.net/qbittorrent/"
+  name "qBittorrent"
+  desc "Peer to peer Bitorrent client"
+  homepage "https://www.qbittorrent.org/"
+
+  depends_on macos: ">= :mojave"
+
+  # Renamed for consistency: app name is different in the Finder and in a shell.
+  app "qbittorrent.app", target: "qBittorrent.app"
+
+  zap trash: [
+    "~/.config/qBittorrent",
+    "~/Library/Application Support/qBittorrent",
+    "~/Library/Caches/qBittorrent",
+    "~/Library/Preferences/org.qbittorrent.qBittorrent.plist",
+    "~/Library/Preferences/qBittorrent",
+    "~/Library/Saved Application State/org.qbittorrent.qBittorrent.savedState",
+  ]
+end

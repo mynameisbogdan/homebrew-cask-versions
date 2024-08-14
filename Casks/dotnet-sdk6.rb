47c65376c666062c38ab1c5264f56d4df2fe8937
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-08-14 19:13:09 +0300

    bump dotnet-sdk6

--- Casks/dotnet-sdk6.rb
+++ Casks/dotnet-sdk6.rb
@@ -2,12 +2,12 @@ cask "dotnet-sdk6" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "6.0.424,c88c088d-fdf3-46f2-bacb-64ea20d20d65,b751a812209385384ec32a3d84124200"
-    sha256 "e508a0f124b5c4cd7454621a60f35e716723f03686c9b85c4cfc7679ba0b5c16"
+    version "6.0.425,4bb697a0-9509-4a48-aace-adf5cdd9dae5,db8114ff1a9117627a19ef3b5e709a05"
+    sha256 "50a127ab788626387236d0a7c8ff9ad208ce506f33409a3877812cf0504d6a9b"
   end
   on_intel do
-    version "6.0.424,0161ccc5-bb74-4d5f-b153-fb9ed1f31531,a3e393ab61522f4b8050cfd2692ad1d6"
-    sha256 "5e37fe1747490b30adc90fd59dce2a00caf635a447a8fb2952a8ba0a3469f903"
+    version "6.0.425,8010eb35-b580-4fe3-84a1-323aef5d6947,cd513767c7ece93a71c69703169dd1c5"
+    sha256 "7994b86068fdc02666947697e0e396232f154832e777940853c3c1baebb80ec3"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"

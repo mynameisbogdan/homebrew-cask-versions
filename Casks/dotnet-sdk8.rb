093b8506ac57f372b9f007dd1b99c27f83eee7c8
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-04-09 21:25:46 +0300

    bump dotnet-sdk8

--- Casks/dotnet-sdk8.rb
+++ Casks/dotnet-sdk8.rb
@@ -1,17 +1,12 @@
 cask "dotnet-sdk8" do
   arch arm: "arm64", intel: "x64"
 
-  on_arm do
-    version "8.0.407,d798274b-2bfe-48d7-8f0f-70b15a0198f0,b2d3de9536ce33aea258211b8ba55c20"
-    sha256 "6cad4e5449a37df79cbd278e0ce2c6b380c3449a708c77d811ba9b1a4383ea0b"
-  end
-  on_intel do
-    version "8.0.407,36cd1a79-ccbe-4e86-b2ea-d4b45aecae1d,baee9ad982dc7c8058ec825978bf2400"
-    sha256 "b09e6c16650da962e98e761c6134c31a04ea92591a40a58ec6dc211edb464e49"
-  end
+  version "8.0.408"
+  sha256 arm:   "1923db0c13780c2cde8fe0e3b9283f1bb772d85f287fd3d7779e12fb92604d83",
+         intel: "d13d66592c17a63ed4cee4a4d6ce3d45f6208d4f7687ad374a234bcd6d299a59"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
-  name ".NET SDK"
+  url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
+  name ".NET SDK 8"
   desc "Developer platform"
   homepage "https://www.microsoft.com/net/core#macos"
 
@@ -19,10 +14,15 @@ cask "dotnet-sdk8" do
   # cask version. New major/minor releases occur annually in November and the
   # check will automatically update its behavior when the cask is updated.
   livecheck do
-    url "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/#{version.major_minor}/releases.json"
-    regex(%r{/download/pr/([^/]+)/([^/]+)/dotnet-sdk-v?(\d+(?:\.\d+)+)-osx-#{arch}\.pkg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
+    url "https://builds.dotnet.microsoft.com/dotnet/release-metadata/#{version.major_minor}/releases.json"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["releases"]&.map do |release|
+        version = release.dig("sdk", "version")
+        next unless version&.match(regex)
+
+        version
+      end
     end
   end
 
@@ -35,19 +35,22 @@ cask "dotnet-sdk8" do
   depends_on macos: ">= :mojave"
 
   pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
-  # binary "/usr/local/share/dotnet/dotnet"
+  binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: [
-              "com.microsoft.dotnet.*",
-              "com.microsoft.netstandard.pack.targeting.*",
-            ],
-            delete:  [
-              "/etc/paths.d/dotnet",
-              "/etc/paths.d/dotnet-cli-tools",
-            ]
-
-  zap trash: [
-    "~/.dotnet",
-    "~/.nuget",
+    "com.microsoft.dotnet.*#{version.major_minor}*#{arch}",
+    "com.microsoft.dotnet.sharedhost*#{arch}",
+    "com.microsoft.netstandard.pack.targeting.*",
   ]
+
+  zap pkgutil: "com.microsoft.dotnet.*",
+      delete:  [
+        "/etc/paths.d/dotnet",
+        "/etc/paths.d/dotnet-cli-tools",
+      ],
+      trash:   [
+        "~/.dotnet",
+        "~/.nuget",
+      ]
 end
+

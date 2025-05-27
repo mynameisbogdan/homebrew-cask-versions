4845f840dc222c416f1553e78b61142d00cf7a08
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-05-27 20:05:15 +0300

    bump dotnet-sdk8

--- Casks/dotnet-sdk8.rb
+++ Casks/dotnet-sdk8.rb
@@ -1,9 +1,9 @@
 cask "dotnet-sdk8" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.409"
-  sha256 arm:   "5934d21c503a965d316bbeeb42c2e339d46a98fa64fba9d70352848dbfa50161",
-         intel: "33929d847f818773fc61b746fbb5c248b428af0b5e7a619cbc362319103eed1c"
+  version "8.0.410"
+  sha256 arm:   "b8576a0fa363ef508c205bcdfb9a61066c28d8c11e953beab289a2df9205d43f",
+         intel: "ad0969a485cbc4f39c1e2a453b6dd3bcab127aa28e8708b8e1e09e7e27feb22a"
 
   url "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version}/dotnet-sdk-#{version}-osx-#{arch}.pkg"
   name ".NET SDK 8"

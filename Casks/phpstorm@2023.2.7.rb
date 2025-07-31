d15e97cebb9c1acb17f57a21dae1f247a4736bf0
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-07-31 22:26:24 +0300

    add phpstorm@2023.2.7

---
+++ Casks/phpstorm@2023.2.7.rb
@@ -0,0 +1,42 @@
+cask "phpstorm@2023.2.7" do
+  arch arm: "-aarch64"
+
+  version "2023.2.7,232.10335.8"
+  sha256 arm:   "c43d43729631579d6108275bfc79648e92274f00c9eb25138a9aa38be42e1cea",
+         intel: "bad65d6c6f7ee0ccd01103cae666fcf6a070c3b1a9d248468c1092d49e783b95"
+
+  url "https://download.jetbrains.com/webide/PhpStorm-#{version.csv.first}#{arch}.dmg"
+  name "JetBrains PhpStorm"
+  desc "PHP IDE by JetBrains"
+  homepage "https://www.jetbrains.com/phpstorm/"
+
+  livecheck do
+    skip "Legacy version"
+  end
+
+  conflicts_with cask: [
+    "phpstorm",
+  ], formula: "dotnet"
+  depends_on macos: ">= :high_sierra"
+
+  app "PhpStorm.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/phpstorm.wrapper.sh"
+  binary shimscript, target: "phpstorm"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/PhpStorm.app/Contents/MacOS/phpstorm' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/consentOptions",
+    "~/Library/Application Support/JetBrains/PhpStorm#{version.major_minor}",
+    "~/Library/Caches/JetBrains/PhpStorm#{version.major_minor}",
+    "~/Library/Logs/JetBrains/PhpStorm#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.PhpStorm.plist",
+    "~/Library/Preferences/jetbrains.jetprofile.asset.plist",
+  ]
+end

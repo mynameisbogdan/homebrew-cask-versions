10a136ddb652cc61ca8f4b67e11ab3d3817d57b1
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-01-31 10:38:31 +0200

    bump mise-bin

--- Formula/mise-bin.rb
+++ Formula/mise-bin.rb
@@ -1,7 +1,7 @@
 class MiseBin < Formula
   desc "Polyglot runtime manager (asdf rust clone)"
   homepage "https://mise.jdx.dev/"
-  version "2026.1.3"
+  version "2026.1.11"
   license "MIT"
 
   livecheck do
@@ -14,12 +14,12 @@ class MiseBin < Formula
   on_macos do
     on_intel do
       url "https://github.com/jdx/mise/releases/download/v#{version}/mise-v#{version}-macos-x64.tar.xz"
-      sha256 "d382c15b018c41e93a6d851db12612519408535b5dbc89a7423156f614f48ee8"
+      sha256 "18f777b233ffbab02b7a7aa51d77a16973d101ca282003c4f0c93523d7d30966"
     end
 
     on_arm do
       url "https://github.com/jdx/mise/releases/download/v#{version}/mise-v#{version}-macos-arm64.tar.xz"
-      sha256 "af73d36be91cbbbe7eb9e3f0e6aa4ea14e9c77d6ed3ffd47d597b9ea28bc0f5b"
+      sha256 "c9f7b39d135f6f7205a2c36f1c6af1eb2b6c587c305163fbee7dc1a863b98c49"
     end
   end
 
